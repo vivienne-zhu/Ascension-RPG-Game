c012ba83e27ee26eb8ce8765d66db887d4ea100c
David Cai <57420559+david-cai@users.noreply.github.com>
2019-11-11 00:30:56 -0500

    Updated Boss functionality

--- src/application/Runner.java
+++ src/application/Runner.java
@@ -32,27 +32,27 @@ public class Runner {
 			int numEnemies = 1 + (int) (Math.random() * ((1) + 1)) + (int) Math.sqrt(floor); 
 		//	int totalEnemyHealth = 0;
 			for (int i = 0; i < numEnemies; i++) {
-				
-				int randEnemy = (int) (Math.random() * ((2) + 1));
-				while (randEnemy == 2 && healerCount == 1) { //max one healer per battle
-					randEnemy = (int) (Math.random() * ((2) + 1));
-				}
-				
-				if (randEnemy == 0) {
-					enemyList.add(new MeleeEnemy(floor));
-				} else if (randEnemy == 1) {
-					enemyList.add(new RangedEnemy(floor));
+				if (floor == 10) {
+					enemyList.add(new BossEnemy(floor));
 				} else {
-					enemyList.add(new HealerEnemy(floor));
-					healerCount++;
+					int randEnemy = (int) (Math.random() * ((2) + 1));
+					while (randEnemy == 2 && healerCount == 1) { //max one healer per battle
+						randEnemy = (int) (Math.random() * ((2) + 1));
+					}
+					
+					if (randEnemy == 0) {
+						enemyList.add(new MeleeEnemy(floor));
+					} else if (randEnemy == 1) {
+						enemyList.add(new RangedEnemy(floor));
+					} else {
+						enemyList.add(new HealerEnemy(floor));
+						healerCount++;
+					}
 				}
 			//	totalEnemyHealth += enemyList.get(i).getCurrentStamina();
 			}
-		
 			
-			
-			//player turn
-			while (player.getCurrentStamina() > 0 && numEnemies > 0 && gameOn) {
+			while (player.getCurrentStamina() > 0 && numEnemies > 0 && gameOn && floor <= 10) {
 				//player turn
 				System.out.println("\nThis is floor: " + floor);
 				System.out.println("\nYour health: " + player.getCurrentStamina());
@@ -81,18 +81,19 @@ public class Runner {
 					
 				}
 
-
 				//enemy turn
 				System.out.println("\nIt is the enemy's turn.");
 				for (int i = 0; i < numEnemies; i++) {
-					int attackAmount = enemyList.get(i).attack(player);
-					System.out.println("The " + enemyList.get(i).getType() + " enemy attacked you!");
-					if (attackAmount <= 0) {
-						System.out.println("The enemy's attack had no effect on you!");
-					} else {
-						System.out.println("Your health is now " + player.getCurrentStamina() + ".");
-						if (player.isDefending()) {
-							System.out.println("Your defense blocked " + attackAmount / 2 + " damage!");
+					if (player.getCurrentStamina() > 0) {
+						int attackAmount = enemyList.get(i).attack(player);
+						System.out.println("The " + enemyList.get(i).getType() + " enemy attacked you!");
+						if (attackAmount <= 0) {
+							System.out.println("The enemy's attack had no effect on you!");
+						} else {
+							System.out.println("Your health is now " + player.getCurrentStamina() + ".");
+							if (player.isDefending()) {
+								System.out.println("Your defense blocked " + attackAmount / 2 + " damage!");
+							}
 						}
 					}
 				}
@@ -106,6 +107,11 @@ public class Runner {
 					floor++;
 					gameOn = true;
 				}
+			}
+			
+			if (floor == 11) {
+				System.out.println("\nCongratulations, you have finished the game!");
+				gameOn = false;
 			}
 			
 
