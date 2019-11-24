ddef34cef16fc6d925b907a74683fabeefa12e05
jiayu-zhu <jiayuzhu@seas.upenn.edu>
2019-11-23 16:10:03 -0800

    Deleted moved out sound effect method

--- src/application/BattlePhase.java
+++ src/application/BattlePhase.java
@@ -773,39 +773,6 @@ public class BattlePhase {
 		SequentialTransition sequence = new SequentialTransition(timeline, sound, enemyRed, hit, magicClear);
 		sequence.play();    	
 	}
-
-//	/**
-//	 * This method plays the sword swing sound effect.
-//	 */
-//	public void swingSound() {
-//		String musicFile = "./src/swing2.wav";
-//		Media sound = new Media(new File(musicFile).toURI().toString());
-//		mediaPlayer = new MediaPlayer(sound);
-//		mediaPlayer.play();
-//		mediaPlayer.setVolume(0.1);
-//	}
-//
-//	/**
-//	 * This method plays the poof sound effect when magic is used.
-//	 */
-//	public void magicSound() {
-//		String musicFile = "./src/fireball.mp3";
-//		Media sound2 = new Media(new File(musicFile).toURI().toString());
-//		mediaPlayer = new MediaPlayer(sound2);
-//		mediaPlayer.play();
-//		mediaPlayer.setVolume(0.06);
-//	}
-//	
-//	/**
-//	 * This method plays a sound when the enemy is killed.
-//	 */
-//	public void enemyDeathSound() {
-//		String musicFile = "./src/enemyDeath.wav";
-//		Media sound3 = new Media(new File(musicFile).toURI().toString());
-//		mediaPlayer = new MediaPlayer(sound3);
-//		mediaPlayer.play();
-//		mediaPlayer.setVolume(0.3);		
-//	}
 	
 	/**
 	 * This method is called when an hero hits an enemy. It is unique
