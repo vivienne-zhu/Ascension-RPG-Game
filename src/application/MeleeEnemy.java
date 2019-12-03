a9200edd0655ea3df8c74468fd3f26db2023a7ab
David Cai <57420559+david-cai@users.noreply.github.com>
2019-12-03 13:19:39 -0500

    Enemy stat modifications (meant to push with others)

--- src/application/MeleeEnemy.java
+++ src/application/MeleeEnemy.java
@@ -18,7 +18,7 @@ public class MeleeEnemy extends GameCharacters {
 	 */
 	public MeleeEnemy(int floor, int position) {
 		// Stat values will be changed
-		setAttack(60 + (3 + (int) (Math.random() * (5 - 3) + 1)) * floor); //60 + (3 to 5 * floor)
+		setAttack(60 + (4 + (int) (Math.random() * (5 - 4) + 1)) * floor); //60 + (4 to 5 * floor)
 		setDefense(45 + (2 + (int) (Math.random() * (4 - 2) + 1)) * floor); //45 + (2 to 4 * floor)
 		int startStam = 300 + 25 * floor;
 		setStamina(startStam);
